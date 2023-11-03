ba99a2c93b0a5145227b036d482dcbc4afd5af57
simusud <simkhadasudeep9@gmail.com>
2023-11-03 15:52:13 +0545

    added local coin token contract

---
+++ contracts/localcoin/src/contract.rs
@@ -0,0 +1,110 @@
+
+use crate::admin::{has_administrator, read_administrator, write_administrator};
+use crate::balance::{read_balance, receive_balance, spend_balance};
+use crate::metadata::{read_decimal, read_name, read_symbol, write_metadata};
+use crate::storage_types::{INSTANCE_BUMP_AMOUNT, INSTANCE_LIFETIME_THRESHOLD};
+use soroban_sdk::{contract, contractimpl, Address, Env, String};
+use soroban_token_sdk::metadata::TokenMetadata;
+use soroban_token_sdk::TokenUtils;
+
+fn check_nonnegative_amount(amount: i128) {
+    if amount < 0 {
+        panic!("negative amount is not allowed: {}", amount)
+    }
+}
+
+#[contract]
+pub struct LocalCoin;
+
+#[contractimpl]
+impl LocalCoin {
+    pub fn initialize(e: Env, admin: Address, decimal: u32, name: String, symbol: String) {
+        if has_administrator(&e) {
+            panic!("already initialized")
+        }
+        write_administrator(&e, &admin);
+        if decimal > u8::MAX.into() {
+            panic!("Decimal must fit in a u8");
+        }
+
+        write_metadata(
+            &e,
+            TokenMetadata {
+                decimal,
+                name,
+                symbol,
+            },
+        )
+    }
+
+    pub fn mint(e: Env, to: Address, amount: i128) {
+        check_nonnegative_amount(amount);
+        let admin = read_administrator(&e);
+        admin.require_auth();
+
+        e.storage()
+            .instance()
+            .bump(INSTANCE_LIFETIME_THRESHOLD, INSTANCE_BUMP_AMOUNT);
+
+        receive_balance(&e, to.clone(), amount);
+        TokenUtils::new(&e).events().mint(admin, to, amount);
+    }
+
+    pub fn set_admin(e: Env, new_admin: Address) {
+        let admin = read_administrator(&e);
+        admin.require_auth();
+
+        e.storage()
+            .instance()
+            .bump(INSTANCE_LIFETIME_THRESHOLD, INSTANCE_BUMP_AMOUNT);
+
+        write_administrator(&e, &new_admin);
+        TokenUtils::new(&e).events().set_admin(admin, new_admin);
+    }
+
+    pub fn balance_of(e: Env, id: Address) -> i128 {
+        e.storage()
+            .instance()
+            .bump(INSTANCE_LIFETIME_THRESHOLD, INSTANCE_BUMP_AMOUNT);
+        read_balance(&e, id)
+    }
+
+    pub fn transfer(e: Env, from: Address, to: Address, amount: i128) {
+        from.require_auth();
+
+        check_nonnegative_amount(amount);
+
+        e.storage()
+            .instance()
+            .bump(INSTANCE_LIFETIME_THRESHOLD, INSTANCE_BUMP_AMOUNT);
+
+        spend_balance(&e, from.clone(), amount);
+        receive_balance(&e, to.clone(), amount);
+        TokenUtils::new(&e).events().transfer(from, to, amount);
+    }
+
+    pub fn burn(e: Env, from: Address, amount: i128) {
+        from.require_auth();
+
+        check_nonnegative_amount(amount);
+
+        e.storage()
+            .instance()
+            .bump(INSTANCE_LIFETIME_THRESHOLD, INSTANCE_BUMP_AMOUNT);
+
+        spend_balance(&e, from.clone(), amount);
+        TokenUtils::new(&e).events().burn(from, amount);
+    }
+
+    pub fn decimals(e: Env) -> u32 {
+        read_decimal(&e)
+    }
+
+    pub fn name(e: Env) -> String {
+        read_name(&e)
+    }
+
+    pub fn symbol(e: Env) -> String {
+        read_symbol(&e)
+    }
+}
---
+++ contracts/localcoin/src/lib.rs
@@ -0,0 +1,7 @@
+#![no_std]
+
+mod admin;
+mod balance;
+mod contract;
+mod metadata;
+mod storage_types;
