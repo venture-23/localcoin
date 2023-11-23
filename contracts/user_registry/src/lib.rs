16ee0d19477530c6824c99b539fcae22d1da446f
simusud <simkhadasudeep9@gmail.com>
2023-11-23 11:36:10 +0545

    feat(user): added merchant verification features

--- contracts/user_registry/src/lib.rs
+++ contracts/user_registry/src/lib.rs
@@ -1,5 +1,5 @@
 #![no_std]
-use soroban_sdk::{contract, contractimpl, contracttype, Address, Env, Vec, vec};
+use soroban_sdk::{contract, contractimpl, contracttype, Address, String, Env, Vec, Val, Map, vec, map};
 
 #[contract]
 pub struct UserRegisrty;
@@ -8,7 +8,8 @@ pub struct UserRegisrty;
 #[contracttype]
 pub enum DataKeys{
     SuperAdmin,
-    MerchantList,
+    VerifiedMerchantList,
+    MerchantsInfo(Address),
     CampaignAdmin(Address)
 }
 
@@ -31,25 +32,57 @@ impl UserRegisrty {
         env.storage().instance().set(&key, &new_super_admin)
     }
 
-    // super admin registers new merchants
-    pub fn on_board_merchants(env:Env, merchant:Address) {
+    pub fn merchant_registration(env:Env, merchant_wallet:Address, proprietor:String, phone_no:String, store_name:String, location:String) {
+        let key = DataKeys::MerchantsInfo(merchant_wallet);
+
+        let merchant_info: Map<String, Val> = map![
+            &env,
+            (String::from_slice(&env, "status"), false.into()),
+            (String::from_slice(&env, "proprietor"), proprietor.to_val()),
+            (String::from_slice(&env, "phone_no"), phone_no.to_val()),
+            (String::from_slice(&env, "store_name"), store_name.to_val()),
+            (String::from_slice(&env, "location"), location.to_val())
+            ];
+        env.storage().instance().set(&key, &merchant_info)
+    }
+
+    pub fn verify_merchant(env:Env, merchant_addr:Address) {
+        // super admin verifies new merchant
         let super_admin = Self::get_super_admin(env.clone());
         super_admin.require_auth();
 
-        let key = DataKeys::MerchantList;
-        let mut merchants_list = Self::get_merchants(env.clone());
-        merchants_list.push_back(merchant);
+        // update merchant status to true
+        let key = DataKeys::MerchantsInfo(merchant_addr.clone());
+        let mut merchant_info = Self::get_merchant_info(env.clone(), merchant_addr.clone());
+        merchant_info.set(String::from_slice(&env, "status"), true.into());
+        env.storage().instance().set(&key, &merchant_info);
+
+        // store a list of merchants
+        let key = DataKeys::VerifiedMerchantList;
+        let mut merchants_list = Self::get_verified_merchants(env.clone());
+        merchants_list.push_back(merchant_addr);
         env.storage().instance().set(&key, &merchants_list)
     }
 
-    // set admin of campaigns
-    pub fn set_campaign_admin(env:Env, campaign:Address, admin:Address) {
+    pub fn set_campaign_admin(env:Env, campaign_management:Address, campaign:Address, admin:Address) {
+        // TODO: check if this auth works
+        campaign_management.require_auth();
+
         let key = DataKeys::CampaignAdmin(campaign);
         env.storage().instance().set(&key, &admin)
     }
 
-    pub fn get_merchants(env:Env) -> Vec<Address> {
-        let key = DataKeys::MerchantList;
+    pub fn get_merchant_info(env:Env, merchant_addr:Address) -> Map<String, Val> {
+        let key = DataKeys::MerchantsInfo(merchant_addr);
+        if let Some(merchant_info) = env.storage().instance().get::<DataKeys, Map<String, Val>>(&key) {
+            merchant_info
+        } else {
+            map![&env]
+        }
+    }
+
+    pub fn get_verified_merchants(env:Env) -> Vec<Address> {
+        let key = DataKeys::VerifiedMerchantList;
         if let Some(merchants_list) = env.storage().instance().get::<DataKeys, Vec<Address>>(&key) {
             merchants_list
         } else {
