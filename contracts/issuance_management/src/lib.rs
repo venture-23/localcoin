8c5d44801ab12df574e0a5418e58203d5c519fc0
simusud <simkhadasudeep9@gmail.com>
2023-11-24 15:43:50 +0545

    fix(issuance): removed tokenlist datakey, added token initialization

--- contracts/issuance_management/src/lib.rs
+++ contracts/issuance_management/src/lib.rs
@@ -20,7 +20,6 @@ mod user_registry {
 pub enum DataKeys{
     UserRegistry,
     SaltCounter,
-    DeployedTokensList,
     ItemsAssociated(Address),
     MerchantsAssociated(Address)
 }
@@ -31,12 +30,12 @@ pub struct IssuanceManagement;
 #[contractimpl]
 impl IssuanceManagement {
     // initialize contract
-    pub fn initialize(env:Env, address:Address) {
+    pub fn initialize(env:Env, user_registry:Address) {
         if Self::has_user_registry(env.clone()) {
             panic!("Contract already initialized.")
         }
         let key = DataKeys::UserRegistry;
-        env.storage().instance().set(&key, &address)
+        env.storage().instance().set(&key, &user_registry)
     }
 
     pub fn set_user_registry(env:Env, address:Address) {
@@ -47,69 +46,80 @@ impl IssuanceManagement {
         env.storage().instance().set(&key, &address)
     }
 
-    pub fn issue_new_token(env:Env, items:Vec<String>, merchants:Vec<Address>) {
-        let super_admin = Self::get_super_admin(env.clone());
-        super_admin.require_auth();
+    pub fn issue_new_token(env:Env, campaign_management:Address, decimal:u32, name:String, symbol:String, 
+        items:Vec<String>, merchants:Vec<Address>) 
+        {
+            let super_admin = Self::get_super_admin(env.clone());
+            super_admin.require_auth();
 
-        let wasm_hash = env.deployer().upload_contract_wasm(localcoin::WASM);
+            let wasm_hash = env.deployer().upload_contract_wasm(localcoin::WASM);
 
-        // dynamic salt count to deploy multiple token contracts
-        let salt_key = DataKeys::SaltCounter;
-        let salt_count: u32 = env.storage().instance().get::<DataKeys, u32>(&salt_key).unwrap_or(0);
-        let salt = BytesN::from_array(&env, &[salt_count.try_into().unwrap(); 32]);
+            // dynamic salt count to deploy multiple token contracts
+            let salt_key = DataKeys::SaltCounter;
+            let salt_count: u32 = env.storage().instance().get::<DataKeys, u32>(&salt_key).unwrap_or(0);
+            let salt = BytesN::from_array(&env, &[salt_count.try_into().unwrap(); 32]);
 
-        let deployed_token = env.deployer().with_address(super_admin, salt).deploy(wasm_hash);
-        
-        // increase salt counter 
-        env.storage().instance().set(&salt_key, &(salt_count + 1));
+            let deployed_token = env.deployer().with_address(super_admin, salt).deploy(wasm_hash);
+            
+            let token_client = localcoin::Client::new(&env, &deployed_token);
+            let current_contract = env.current_contract_address();
 
-        // store items associated with token
-        let item_key = DataKeys::ItemsAssociated(deployed_token.clone());
-        env.storage().instance().set(&item_key, &items);
-        
-        // store merchants associated with token
-        let merchant_key = DataKeys::MerchantsAssociated(deployed_token.clone());
-        env.storage().instance().set(&merchant_key, &merchants);
+            // set issuance management contract in deployed token 
+            token_client.set_issuance_management(&current_contract);
+            // initialize deployed token
+            token_client.initialize(&campaign_management, &decimal, &name, &symbol);
 
-        // store deployed tokens 
-        let key = DataKeys::DeployedTokensList;
-        let mut existing_tokens =  Self::get_available_tokens(env.clone());
-        existing_tokens.push_back(deployed_token.clone());
-        env.storage().instance().set(&key, &existing_tokens);
+            // increase salt counter 
+            env.storage().instance().set(&salt_key, &(salt_count + 1));
 
-        // set issuance management contract in above deployed token contract
-        let token_client = localcoin::Client::new(&env, &deployed_token);
-        let current_contract = env.current_contract_address();
-        token_client.set_issuance_management(&current_contract);
+            // store items associated with token
+            let item_key = DataKeys::ItemsAssociated(deployed_token.clone());
+            env.storage().instance().set(&item_key, &items);
+            
+            // store merchants associated with token
+            let merchant_key = DataKeys::MerchantsAssociated(deployed_token.clone());
+            env.storage().instance().set(&merchant_key, &merchants);
+
+            //  send deployed tokens to user_registry contract
+            let user_registry_addr = Self::get_user_registry(env.clone());
+            let user_registry_client = user_registry::Client::new(&env, &user_registry_addr);
+            user_registry_client.add_deployed_tokens(&deployed_token);
     }
 
     // adds new items for an existing token
-    pub fn add_token_items(env:Env, token_addr:Address, items:Vec<String>) {
+    pub fn add_token_items(env:Env, token_address:Address, items:Vec<String>) {
         let super_admin = Self::get_super_admin(env.clone());
         super_admin.require_auth();
 
-        let key = DataKeys::ItemsAssociated(token_addr.clone());
+        let key = DataKeys::ItemsAssociated(token_address.clone());
+        if !env.storage().instance().has(&key) {
+            panic!("Token doesn't exist.")
+        }
 
-        let existig_items = Self::get_items_assocoated(env.clone(), token_addr);
+        let existig_items = Self::get_items_assocoated(env.clone(), token_address);
         let updated_items_list = vec![&env, existig_items, items].concat();
         env.storage().instance().set(&key, &updated_items_list);
     }
 
-    // adds new merchants for an existing topken
-    pub fn add_token_merchants(env:Env, token_addr:Address, merchants:Vec<Address>) {
+    // adds new merchants for an existing token
+    pub fn add_token_merchants(env:Env, token_address:Address, merchants:Vec<Address>) {
         let super_admin = Self::get_super_admin(env.clone());
         super_admin.require_auth();
 
-        let key = DataKeys::MerchantsAssociated(token_addr.clone());
+        let key = DataKeys::MerchantsAssociated(token_address.clone());
+        if !env.storage().instance().has(&key) {
+            panic!("Token doesn't exist.")
+        }
 
-        let existig_merchants = Self::get_merchants_assocoated(env.clone(), token_addr);
+        let existig_merchants = Self::get_merchants_assocoated(env.clone(), token_address);
         let updated_merchants_list = vec![&env, existig_merchants, merchants].concat();
         env.storage().instance().set(&key, &updated_merchants_list);
     }
 
-    // returns a map of token and its balance of given address
     pub fn get_balance_of_batch(env:Env, user:Address) -> Map<String, i128> {
-        let tokens =  Self::get_available_tokens(env.clone());
+        let user_registry_addr = Self::get_user_registry(env.clone());
+        let user_registry_client = user_registry::Client::new(&env, &user_registry_addr);
+        let tokens =  user_registry_client.get_available_tokens();
         
         let mut tokens_balance: Map<String, i128> = Map::new(&env);
         for token in tokens.iter() {
@@ -123,16 +133,6 @@ impl IssuanceManagement {
         tokens_balance
     }
 
-    // returns list of tokens available
-    pub fn get_available_tokens(env:Env) -> Vec<Address> {
-        let key = DataKeys::DeployedTokensList;
-        if let Some(tokens) = env.storage().instance().get::<DataKeys, Vec<Address>>(&key) {
-            tokens
-        } else {
-            vec![&env]
-        }
-    }
-
     pub fn get_user_registry(env:Env) -> Address {
         let key = DataKeys::UserRegistry;
         if let Some(user_registry_addr) = env.storage().instance().get::<DataKeys, Address>(&key) {
@@ -144,8 +144,8 @@ impl IssuanceManagement {
 
     pub fn get_super_admin(env:Env) -> Address {
         let user_registry_addr = Self::get_user_registry(env.clone());
-        let client = user_registry::Client::new(&env, &user_registry_addr);
-        client.get_super_admin()
+        let user_registry_client = user_registry::Client::new(&env, &user_registry_addr);
+        user_registry_client.get_super_admin()
     }
 
     pub fn get_merchants_assocoated(env:Env, token_address:Address) -> Vec<Address> {
